2d1979d2e094b64615661dc2f0f88a911bf4a5ae
klousable <carrieleung@live.ca>
2023-04-12 16:07:05 -0400

    ADD: further formatting, hiring check function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
-console.log("SCRIPT LINKED");
+const me = document.getElementById("me");
+const realme = document.getElementById("realme");
+const metext = document.getElementById("me-text");
 
-const me = document.getElementById('me');
-const realme = document.getElementById('realme'); 
-const metext = document.getElementById('me-text');
-realme.addEventListener('click', function() {
-    if (realme.textContent === "The real me(?)") {
-        me.src = "media/therealme.jpg";
-        metext.innerHTML = "Turns out you can't tell if someone is a cat on the internet.";
-        realme.textContent = "Back to the fake me";
-        me.id = "realme-image";
-      } else {
-        me.src = "media/me.jpg";
-        realme.textContent = "The real me(?)";
-        metext.innerHTML = "A pretty recent photo of me.";
-        me.id = "me";
-      }
+const form = document.forms["contactForm"];
+const hiring = form.elements["about"][2]; 
+const hourly = document.getElementById("hourly-rate");
+
+/* For fun function that swaps out my portfolio picture with the extra button */
+realme.addEventListener("click", function () {
+  if (realme.textContent === "The real me(?)") {
+    me.src = "media/therealme.jpg";
+    metext.innerHTML =
+      "Turns out you can't tell if someone is a cat on the internet.";
+    realme.textContent = "Back to the fake me";
+    me.id = "realme-image";
+  } else {
+    me.src = "media/me.jpg";
+    realme.textContent = "The real me(?)";
+    metext.innerHTML = "A pretty recent photo of me.";
+    me.id = "me";
+  }
 });
+
+/* Hiring Form */ 
+
+form.addEventListener("change", function() {
+  if (hiring.checked) {
+    hourly.style.display = "block";
+    hourly.querySelector("input").setAttribute("required", true);
+  } else {
+    hourly.style.display = "none";
+    hourly.querySelector("input").removeAttribute("required");
+  }
+});
+
+/* Form Validation */
 
