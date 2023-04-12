f19cb488140a3db2eacd419a7a91ee6a70fa4fc1
klousable <carrieleung@live.ca>
2023-04-11 23:43:57 -0400

    ADD: media, index basic formatting and .js mod

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("SCRIPT LINKED");
+
+const me = document.getElementById('me');
+const realme = document.getElementById('realme'); 
+const metext = document.getElementById('me-text');
+realme.addEventListener('click', function() {
+    if (realme.textContent === "The real me(?)") {
+        me.src = "media/therealme.jpg";
+        metext.innerHTML = "Turns out you can't tell if someone is a cat on the internet.";
+        realme.textContent = "Back to the fake me";
+        me.id = "realme-image";
+      } else {
+        me.src = "media/me.jpg";
+        realme.textContent = "The real me(?)";
+        metext.innerHTML = "A pretty recent photo of me.";
+        me.id = "me";
+      }
+});
+
